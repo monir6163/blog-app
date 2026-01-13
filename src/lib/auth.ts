1c8e7b00f8a37b8b085279c7954b17d4b4bf60b9
monir6163 <monirhossain6163@gmail.com>
2026-01-13 13:34:36 +0600

    nodemailer mail send

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,26 +1,31 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { UserStatus } from "../../generated/prisma/enums";
+import { transporter } from "./mailService";
 import { prisma } from "./prisma";
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  // Add all possible origins for web, mobile, and testing environments
+  trustedOrigins: [process.env.CLIENT_URL || "http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
     autoSignIn: false,
     requireEmailVerification: true,
   },
-  // Add all possible origins for web, mobile, and testing environments
-  trustedOrigins: [process.env.CLIENT_URL || "http://localhost:3000"],
-
   // email verification
   emailVerification: {
     sendVerificationEmail: async ({ user, url, token }, request) => {
-      console.log({ user, url, token });
+      const info = await transporter.sendMail({
+        from: "prisma blog <info@nexalogictech.com>",
+        to: user.email,
+        subject: "Email Verify",
+        html: `<b>Hello World</b>`,
+      });
+      console.log(`${info.messageId}`);
     },
   },
-
   // extendable additionalfield user schema
   user: {
     additionalFields: {
