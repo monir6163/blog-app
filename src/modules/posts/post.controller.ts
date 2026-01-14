62f7e5119f3c76f6cd8f08abee6c3056b1cc89a3
monir6163 <monirhossain6163@gmail.com>
2026-01-14 16:09:34 +0600

    dynamic authorid

--- src/modules/posts/post.controller.ts
+++ src/modules/posts/post.controller.ts
@@ -1,17 +1,15 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 import { postService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
-    // Implementation for creating a post will go here
-    // res.send("Create a new post");
-    // console.log(req, res)
-
-    try{
-        const result = await postService.createPost(req.body);
-        res.status(201).json(result);
-    }catch(err){
-        res.status(500).json({ error: "Failed to create post" });
-    }
-}
+  try {
+    const bodyData = req.body;
+    bodyData.authorId = req.user?.id;
+    const result = await postService.createPost(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to create post" });
+  }
+};
 
 export const postController = { createPost };
